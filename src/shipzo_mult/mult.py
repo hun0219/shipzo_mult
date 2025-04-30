a3d661699d70e35e9d545a6f9068494750a97407
hun0219 <gsw98000@gmail.com>
2025-04-30 14:48:24 +0900

    mult 값확인

--- src/shipzo_mult/mult.py
+++ src/shipzo_mult/mult.py
@@ -1,10 +1,10 @@
 #곱셈
 import sys
 
-def plus(a: int, b: int):
+def mult(a: int, b: int):
     print(a * b)
 
 def main():
     args = sys.argv[1:]
     a, b = int(args[0]), int(args[1])
-    plus(a, b)
+    mult(a, b)
